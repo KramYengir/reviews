5ccaa0c3c56b28d2ac9259fc9377e7bb4746fe89
KramYengir <kramyengir@gmail.com>
2024-03-18 15:19:24 +0000

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import Header from "./components/Header";
+import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
+import Homepage from "./pages/Homepage";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/review/:id" element={<ReviewDetails />} />
+          <Route path="/category/:id" element={<Category />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
