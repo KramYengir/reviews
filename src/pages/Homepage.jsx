3645f9544a36c64f95177415aeb04d8dac0db77d
KramYengir <kramyengir@gmail.com>
2024-03-18 17:41:43 +0000

    truncate review body & some styling

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import usefetch from "../hooks/useFetch";
+
+const URL = "http://localhost:1337/api/reviews";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const { data, loading, error } = usefetch(URL);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Errrrrrror...</p>;
+
+  return (
+    <>
+      {data.data &&
+        data.data.map((review) => (
+          <div key={review.id} className="review-card">
+            <div className="rating">{review.attributes.rating}</div>
+            <h2>{review.attributes.title}</h2>
+            <small>console list</small>
+            <p>
+              {review.attributes.body[0].children.map((child) =>
+                child.text.substring(0, 200)
+              )}
+              ...
+            </p>
+            <Link to={`/details/${review.id}`}>Read More</Link>
+          </div>
+        ))}
+    </>
+  );
 };
 
 export default Homepage;
