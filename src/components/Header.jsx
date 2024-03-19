7558ed991502c1abce567323f08836c40e2de1bc
KramYengir <kramyengir@gmail.com>
2024-03-19 15:03:56 +0000

    add & style category nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const CATEGORIES = gql`
+  query GetCategories {
+    categories {
+      data {
+        id
+        attributes {
+          name
+        }
+      }
+    }
+  }
+`;
 
 const Header = () => {
+  const { loading, error, data } = useQuery(CATEGORIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Errrrrrror...</p>;
+
+  console.log("category: ", data);
+
   return (
     <header>
       <Link to="/">
         <h1>Mark Reviews</h1>
       </Link>
+      <nav className="categories">
+        <span>Filter reviews by category: </span>
+        {data.categories.data.map((category) => (
+          <Link key={category.id} to={`/category/${category.id}`}>
+            {category.attributes.name}{" "}
+          </Link>
+        ))}
+      </nav>
     </header>
   );
 };
