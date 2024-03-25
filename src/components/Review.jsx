67c435e05459a5ca0e288e29267132ea8318ee48
KramYengir <kramyengir@gmail.com>
2024-03-25 13:52:04 +0000

    add spaces between review paragraphs

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import Markdown from "react-markdown";
 
 const Review = ({ rating, title, categories, body, link }) => {
-  const ratingStyle = () => {
-    if (rating >= 9) return "masterpiece";
-    else if (rating < 9 && rating >= 7) return "good";
-    else if (rating < 7 && rating > 4) return "average";
-    else return "terrible";
-  };
+  // const ratingStyle = () => {
+  //   if (rating >= 9) return "masterpiece";
+  //   else if (rating < 9 && rating >= 7) return "good";
+  //   else if (rating < 7 && rating > 4) return "average";
+  //   else return "terrible";
+  // };
 
   return (
     <article className={`review-card ${rating >= 9 && "masterpiece"}`}>
-      <div className={`rating ${ratingStyle()}`}>{rating}</div>
+      <div className="rating">{rating}</div>
       <h2>{title}</h2>
       {categories.map((category) => (
         <small key={category.id}>{category.attributes.name}</small>
       ))}
-      <p>{link ? body.substring(0, 400) + "..." : body}</p>
+      <Markdown>{link ? body.substring(0, 400) + "..." : body}</Markdown>
       {link && <Link to={`/review/${link}`}>Read More</Link>}
     </article>
   );
@@ -33,17 +34,7 @@ Review.propTypes = {
       }).isRequired,
     })
   ).isRequired,
-  body: PropTypes.arrayOf(
-    PropTypes.shape({
-      type: PropTypes.string.isRequired,
-      children: PropTypes.arrayOf(
-        PropTypes.shape({
-          text: PropTypes.string.isRequired,
-          type: PropTypes.string.isRequired,
-        })
-      ).isRequired,
-    })
-  ).isRequired,
+  body: PropTypes.string.isRequired,
   link: PropTypes.string,
 };
 
