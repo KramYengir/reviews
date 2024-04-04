43f051ed850ec8818d36b9dd881142eeedb31ae3
KramYengir <kramyengir@gmail.com>
2024-04-04 16:45:40 +0100

    add hamburger menu & dropdown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,48 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useFilter } from "../../context/FilterContext";
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { setActiveFilter } = useFilter();
-  const { id } = useParams();
+
+  isMenuOpen
+    ? document.body.classList.add("disable-scrolling")
+    : document.body.classList.remove("disable-scrolling");
 
   return (
     <header className="container">
-      <nav className={id ? "hidden" : "categories"}>
+      <nav>
         <Link to="/" onClick={() => setActiveFilter(null)}>
           <h1>Rigney Reviews</h1>
         </Link>
+
+        {isMenuOpen ? (
+          <button aria-expanded={isMenuOpen}>
+            <IoCloseSharp onClick={() => setIsMenuOpen(false)} />
+          </button>
+        ) : (
+          <button aria-expanded={isMenuOpen}>
+            <IoMenu onClick={() => setIsMenuOpen(true)} />
+          </button>
+        )}
+
+        <ul aria-hidden={!isMenuOpen}>
+          <li>
+            <a href="#">Masterpieces</a>
+          </li>
+          <li>
+            <a href="#">Hidden Gems</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Get In Touch</a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
